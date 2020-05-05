5e4c60aba9d48b62b44a67886615a02de0436502
João Felipe <jfpessanha@stefanini.com>
2020-05-05 06:37:50 -0300

    Adds global exception handler

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -2,6 +2,8 @@ import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
 
+import AppError from '../errors/AppError';
+
 import User from '../models/User';
 
 import authConfig from '../config/auth';
@@ -27,13 +29,13 @@ class AuthenticateUserService {
     });
 
     if (!user) {
-      throw new Error('Invalid Email/Password.');
+      throw new AppError('Invalid Email/Password.', 401);
     }
 
     const passwordMatched = await compare(password, user.password);
 
     if (!passwordMatched) {
-      throw new Error('Invalid Email/Password.');
+      throw new AppError('Invalid Email/Password.', 401);
     }
 
     const { secret, expiresIn } = authConfig.jwt;
--- src/services/UpdateUserAvatarService.ts
+++ src/services/UpdateUserAvatarService.ts
@@ -3,6 +3,8 @@ import path from 'path';
 import fs from 'fs';
 import User from '../models/User';
 
+import AppError from '../errors/AppError';
+
 import uploadConfig from '../config/upload';
 
 interface Request {
@@ -17,7 +19,7 @@ class UpdateUserAvatarService {
     const user = await usersRepository.findOne(user_id);
 
     if (!user) {
-      throw new Error('Only authenticated users can change avatar.');
+      throw new AppError('Only authenticated users can change avatar.', 401);
     }
 
     if (user.avatar) {
